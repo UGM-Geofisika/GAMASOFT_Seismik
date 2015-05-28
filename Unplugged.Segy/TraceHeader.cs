318f735c3aa9bfc88b34e6ac44e98efda478be6a
Adien Akhmad <get.add.ins@gmail.com>
2015-05-28 16:15:46 +0700

    Implement ITraceHeader

--- Unplugged.Segy/TraceHeader.cs
+++ Unplugged.Segy/TraceHeader.cs
@@ -7,5 +7,360 @@ namespace Unplugged.Segy
         public int TraceNumber { get; set; }
         public int InlineNumber { get; set; }
         public int CrosslineNumber { get; set; }
+
+        public int Tracl
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Tracr
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Fldr
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Tracf
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Ep
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Cdp
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Cdpt
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Trid
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Nvs
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Nhs
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Duse
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Offset
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Gelev
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Selev
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Sdepth
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Gdel
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Sdel
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Swdep
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Gwdep
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Scalel
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Scalco
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Sx
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Sy
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Gx
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Gy
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Counit
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Wevel
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Swevel
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Sut
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Gut
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Sstat
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Gstat
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Tstat
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Laga
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Lagb
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Delrt
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Muts
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Mute
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Ns
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Dt
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Gain
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Igc
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Igi
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Corr
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Sfs
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Sfe
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Slen
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Styp
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Stas
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Stae
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Tatyp
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Afilf
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Afils
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Nofilf
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Nofils
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Lcf
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Hcf
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Lcs
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Hcs
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Year
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Day
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Hour
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Min
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Sec
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Timbas
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Trwf
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Grnors
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Grnofr
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Grnlof
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Gaps
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int Otrav
+        {
+            get { throw new System.NotImplementedException(); }
+        }
     }
 }
